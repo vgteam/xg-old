1b134bb97914b61cd33aa3ef4bbb6982323445b5
yoheirosen <yrosen@berkeley.edu>
2016-12-13 11:22:11 -0800

    define thread_t in cpp file

--- src/xg.cpp
+++ src/xg.cpp
@@ -2299,6 +2299,8 @@ int64_t XG::where_to(int64_t current_side, int64_t visit_offset, int64_t new_sid
     return new_visit_offset;
 }
 
+using thread_t = vector<ThreadMapping>;
+
 thread_t extract_thread(xg::XG::ThreadMapping node, int64_t offset = 0, int64_t max_length = 0) {
   thread_t path;
   int64_t side = (node.node_id)*2 + node.is_reverse;
